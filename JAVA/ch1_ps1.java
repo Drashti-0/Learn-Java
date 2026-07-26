ba30d8dfd4c947dbb0aa9c0f368a2854f09901f6
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-07-26 21:00:34 +0530

    some chnage

---
+++ JAVA/ch1_ps1.java
@@ -0,0 +1,12 @@
+/* package com.company; */
+
+public class ch1_ps1 {
+
+    public static void main(String[] args) {
+        int a = 4, b = 7, c = 6;
+
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
+
+}
