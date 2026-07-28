1ee52b75b6692eeb1250203148ea211f065a8808
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-07-28 21:11:44 +0530

    add file

---
+++ JAVA/Recursion_facto.java
@@ -0,0 +1,21 @@
+public class Recursion_facto{
+
+    static int facto(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+
+        else{
+            return n * facto(n-1);
+        }
+
+    }
+
+
+public static void main(String[] args){
+
+int x=5;
+System.out.println("The value  of factorial" + facto(x));
+}
+
+}
