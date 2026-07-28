7b8d0ab16d29b3493949c6abf5b893e2f620ee6e
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-07-28 21:09:18 +0530

    add file

---
+++ JAVA/methods.java
@@ -0,0 +1,11 @@
+public class methods{
+    static void telljoke(){
+        System.out.println("I invite a new world !!\n"+"plagirisum");
+    }
+
+
+    public static void main(String[] args){
+        telljoke();
+
+    }
+}
