65003046b5783b01648804fdb2820061b949be58
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-11 22:01:39 +0530

    add this java file practice

---
+++ JAVA/largest.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class largest{
+
+public static void main(String[] args) {
+
+    Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter YOUR Number: ");
+    int a=sc.nextInt();
+
+int d=3,D=113,m=39;
+
+if(d>D &&d>m){
+    System.out.println("LARGEST IS drashti [d]");
+}
+
+else if(D>d && D>m){
+    System.out.println("LAREST ID Dhruti [D]");
+
+}
+
+else{
+    System.out.println("LARGEST is Mantr [m]");
+}
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+}
+
+
