2595dfe6eedf53bdda350f3581299772a600dc1c
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-11 22:03:43 +0530

    add file

---
+++ JAVA/personal_information.java
@@ -0,0 +1,19 @@
+public class personal_information{
+
+    public static void main(String[] args){
+
+        System.out.println("Drashti Bhanderi");
+
+        System.out.print("I am student at cspit institute");
+
+        System.out.print("25CS003");
+
+
+
+    }
+
+
+
+
+
+};
