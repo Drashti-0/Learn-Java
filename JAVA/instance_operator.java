a882c1144c5641f85805e11502fffb95d973209f
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-17 21:27:05 +0530

    add java practice

---
+++ JAVA/instance_operator.java
@@ -0,0 +1,21 @@
+public class instance_operator {
+
+
+    public static void main(String[] args) {
+
+        instance_operator obj = new instance_operator();
+
+
+        if (obj instanceof instance_operator) {
+            System.out.println("Drashti ");
+        } 
+        
+        else {
+            System.out.println("Dhruti ");
+        }
+
+
+    }
+
+
+}
