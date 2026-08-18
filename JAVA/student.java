bf7d8cd4635f90de3547a6f32d466c6b008b0953
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-18 19:29:22 +0530

    add file

---
+++ JAVA/student.java
@@ -0,0 +1,39 @@
+
+/* sem_reference_pointer */
+
+
+
+public class student{
+
+int s1=20;
+int s2=30;
+
+
+
+public static void main(String[] args) {
+
+  student s1=new student();
+  student s2=s1;  
+
+  System.out.println(s1.s1);  
+  System.out.println(s2.s1);
+
+
+
+
+
+
+
+
+
+    
+  }
+
+
+
+
+
+
+
+
+}
