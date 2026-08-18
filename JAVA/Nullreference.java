0ca15b8b7b21a731d7890d0c68e43a590c04c4ef
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-18 19:27:01 +0530

    add file

---
+++ JAVA/Nullreference.java
@@ -0,0 +1,13 @@
+public class NullReference {
+
+    void display() {
+        System.out.println("Hello Java");
+    }
+
+    public static void main(String[] args) {
+
+        NullReference obj = null;
+
+        obj.display();
+    }
+}
