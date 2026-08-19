901d27fe446404f7f0b9f11bbd3499b970c16e5f
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-19 22:19:26 +0530

    add java practice

---
+++ JAVA/defualt.java
@@ -0,0 +1,12 @@
+class defualttt {
+    void display() {
+        System.out.println("Student");
+    }
+}
+
+public class defualt {
+    public static void main(String[] args) {
+
+        defualttt obj = new defualttt();
+    }
+}
