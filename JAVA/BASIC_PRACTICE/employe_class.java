5571d2322d2447936a46f5f1184f50fee3624775
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-25 23:08:35 +0530

    add code file

---
+++ JAVA/BASIC_PRACTICE/employe_class.java
@@ -0,0 +1,17 @@
+public class employe_class{
+    
+    String name;
+    int salary;
+
+    public static void main(){
+
+employe_class s1 = new employe_class();
+
+s1.name = "DRASHTI ";
+s1.salary=500000;
+
+System.out.println("NAME : " + s1.name);
+System.out.println("SALARY : " + s1.salary);
+
+    }
+}
