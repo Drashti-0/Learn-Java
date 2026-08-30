3dc4588584dec487661632bce6c502a866f76648
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-30 10:21:39 +0530

    add code file

---
+++ Final_Method.java
@@ -0,0 +1,14 @@
+public class Final_Method {
+    
+final void display(){
+  System.out.print("hieeee");
+}
+
+    public static void main(String[] s){
+
+        /* void display(){
+            System.out.print("for change");
+        } */ /* gives error */
+
+    }
+}
