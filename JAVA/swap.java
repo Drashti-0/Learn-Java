35f3f99491adc0dfc7ce5cb93dfe6929ffee345f
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-09-07 22:40:41 +0530

    add file

---
+++ JAVA/swap.java
@@ -0,0 +1,25 @@
+public class swap{
+
+
+public static void main(String[] args){
+
+
+int a=4,b=3;
+
+int temp =a;
+a=b;
+b=temp;
+
+System.out.println("After swapping: ");
+
+System.out.println(" a is " +a);
+
+System.out.println("b is " + b);
+
+
+
+}
+
+
+
+}
