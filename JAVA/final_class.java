3c5a2316ef3b0a909177e06a8db0fdd571fa1a12
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-09-15 20:34:13 +0530

    add file

---
+++ JAVA/final_class.java
@@ -0,0 +1,15 @@
+final class Bank {
+
+    void display() {
+        System.out.println("This is a final class");
+    }
+}
+
+public class final_class{
+    public static void main(String[] args) {
+
+        Bank obj = new Bank();
+
+        obj.display();
+    }
+}
