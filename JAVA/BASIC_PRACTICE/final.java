46fd0a9c231ef84924968ad43dea3fdfb96454d3
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-09-18 22:01:06 +0530

    add file

---
+++ JAVA/BASIC_PRACTICE/final.java
@@ -0,0 +1,10 @@
+public class final {
+    public static void main(String[] args) {
+
+        final double PI = 3.14;
+
+        System.out.println("Value of PI = " + PI);
+
+        // PI = 3.1415;   // Error
+    }
+}
